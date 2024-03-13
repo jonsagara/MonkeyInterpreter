efe6d0ac6cf0644b4c2391feb2ad7cb1c8db2845
Jon Sagara <jon@sagarasoftware.com>
2024-03-13 13:20:38 -0700

    CH01: Add a basic REPL that exercises the lexer.

---
+++ src/Chapter01/MonkeyInterpreter.Repl/Program.cs
@@ -0,0 +1,21 @@
+﻿using MonkeyInterpreter;
+
+const string Prompt = ">> ";
+
+Console.WriteLine("Hello! This is the Monkey programming language!");
+Console.WriteLine("Feel free to type in commands");
+
+string? input = null;
+Console.Write(Prompt);
+
+while ((input = Console.ReadLine()) != null)
+{
+    var lexer = new Lexer(input);
+
+    for (var token = lexer.NextToken(); token.TokenType != TokenTypes.EOF; token = lexer.NextToken())
+    {
+        Console.WriteLine($"{token}");
+    }
+
+    Console.Write(Prompt);
+}
